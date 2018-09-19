85c7e5db42e620674fb2754f1859791ffa099d89
jasonwilson88 <jasonwilson37@gmail.com>
2018-09-19 14:27:10 -0500

    license

--- Part1/SparkWordCount/wordcount.py
+++ Part1/SparkWordCount/wordcount.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     counts = lines.flatMap(lambda x: x.split(' ')) \
                   .map(lambda x: (x, 1)) \
                   .reduceByKey(add)
-    
+ ## modified output to write to hdfs file   
     counts.saveAsTextFile("hdfs:///user/path")
 
     spark.stop()
